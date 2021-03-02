3d61079b04572fbf22e4bd51d5bb832125111217
watchman <watchman@dyn005211.shef.ac.uk>
2021-03-02 10:31:53 +0000

    kill script and cleaned up timer input

---
+++ kill18.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./sethv/sethv --power 0 0
+./sethv/sethv --power 1 0
+./sethv/sethv --power 2 0
+./sethv/sethv --power 3 0
+./sethv/sethv --power 4 0
+./sethv/sethv --power 5 0
+./sethv/sethv --power 6 0
+./sethv/sethv --power 7 0
+./sethv/sethv --power 8 0
+./sethv/sethv --power 9 0
+./sethv/sethv --power 10 0
+./sethv/sethv --power 11 0
+./sethv/sethv --power 12 0
+./sethv/sethv --power 13 0
+./sethv/sethv --power 14 0
+./sethv/sethv --power 15 0
+./sethv/sethv --power 16 0
+./sethv/sethv --power 17 0
+
