87ac11d2d7df5d15471180f8e1952a84911813fc
stw4198 <stw4198@aol.com>
2021-04-10 10:38:09 +0100

    include config file

--- DarkRateTest/DarkRateTest.sh
+++ DarkRateTest/DarkRateTest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(sleep 3s && echo "s" && sleep 3s && echo "W" && sleep 5s && echo "s" && sleep 3s && echo "q") | wavedump
+(sleep 3s && echo "s" && sleep 3s && echo "W" && sleep 900s && echo "s" && sleep 3s && echo "q") | wavedump
