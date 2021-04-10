de4948b6da0650d8323c8ee5ae584669698ca863
Steve Wilson <stw4198@aol.com>
2021-04-10 11:00:45 +0100

    added config file to test dir

--- script/README.md
+++ script/README.md
@@ -2,4 +2,4 @@ Write the shell script to set the testing time in Wavedump
 
 ./script [test] [time]
 
-e.g. ./script DarkRate 900 is a 900 s test using the shell script inside PMT_Testing/DarkRateTest
+e.g. ./script DarkRateTest 900 is a 900 s test using the shell script inside PMT_Testing/DarkRateTest
--- script/script.cpp
+++ script/script.cpp
@@ -9,14 +9,28 @@ int main(int argc, char** argv) {
   int time = atoi(argv[2]);
   
   std::string file = test + "/" + test + ".sh";
-   
+  
+  if(test == "DarkRateTest"){
+  
+  std::string config = "WaveDumpConfig_DarkRate.txt";
+  
   std::ofstream shell;
   shell.open (file);
-  shell << "#!/bin/bash\n\n(sleep 3s && echo \"s\" && sleep 3s && echo \"W\" && sleep " << time << "s && echo \"s\" && sleep 3s && echo \"q\") | wavedump";
+  shell << "#!/bin/bash\n\n(sleep 3s && echo \"s\" && sleep 3s && echo \"W\" && sleep " << time << "s && echo \"s\" && sleep 3s && echo \"q\") | wavedump " << config;
   shell.close();
 
   printf("\nBash script written\n");
 
   return 0;
+  
+  }
+  
+  else{
+  
+  std::cout << "\nNo test folder defined for " << test << "\n";
+  
+  return 1;
+  
+  }
   
 }
